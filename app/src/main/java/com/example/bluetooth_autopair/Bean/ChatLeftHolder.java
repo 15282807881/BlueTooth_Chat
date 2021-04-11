fd10c7b1cc2e3351edbdf0e11f78a69840ccc62f
15282807881 <1261729204@qq.com>
2021-04-11 15:09:03 +0800

    complete

--- app/src/main/java/com/example/bluetooth_autopair/Bean/ChatLeftHolder.java
+++ app/src/main/java/com/example/bluetooth_autopair/Bean/ChatLeftHolder.java
@@ -1,4 +1,33 @@
 package com.example.bluetooth_autopair.Bean;
 
-public class ChatLeftHolder {
+import android.view.View;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.bluetooth_autopair.R;
+public class ChatLeftHolder extends RecyclerView.ViewHolder {
+
+    private TextView tvContent;
+    private TextView tvName;
+
+    public ChatLeftHolder(View itemView) {
+        super(itemView);
+        tvContent = (TextView) itemView.findViewById(R.id.tv_left);
+        tvName = (TextView) itemView.findViewById(R.id.tv_device);
+    }
+
+    public TextView getTvContent() {
+        return tvContent;
+    }
+
+    public void setTvContent(TextView tvContent) {
+        this.tvContent = tvContent;
+    }
+
+    public TextView getTvName() {
+        return tvName;
+    }
+
+    public void setTvName(TextView tvName) {
+        this.tvName = tvName;
+    }
 }
